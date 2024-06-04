8edbc956e3874041f4477cf78f4f12be2ab1d95b
subat <ksw1010100@gmail.com>
2024-06-04 13:01:19 +0900

    first commit

---
+++ src/main/java/kr/co/clover/SeoulTourApplication.java
@@ -0,0 +1,13 @@
+package kr.co.clover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulTourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/clover/SeoulTourApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.clover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoulTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
